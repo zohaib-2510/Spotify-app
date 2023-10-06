023375f86d3a9e1b189c364e965de8c52f46f457
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-06 05:11:25 -0700

    pause play work

--- app.js
+++ app.js
@@ -1,31 +1,111 @@
 console.log("WelCome To Spotify");
 // var
 let songIndex = 0;
-let audioElement = new Audio('1.mp3')
+let audioElement = new Audio('songs/1.mp3')
 let masterPlay = document.getElementById("masterPlay")
 let myProgressbar = document.getElementById("myProgressbar")
-
+let gif = document.getElementById("gif")
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
 // var
 let songs = [
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
-{songName:"Karachi Chal",filePath:"song/1.mp3",coverPath:"covers/1.jpg"}
-]
+{songName:"Karachi Chal - Talha-Younus",filePath:"songs/1.mp3",coverPath:"covers/1.jpg"},
+{songName:"Karachi Mera - Talha-Younus",filePath:"songs/2.mp3",coverPath:"covers/2.jpg"},
+{songName:"Shub  -  Shub",filePath:"songs/3.mp3",coverPath:"covers/3.jpg"},
+{songName:"Divine  -  Divine",filePath:"songs/4.mp3",coverPath:"covers/4.jpg"},
+{songName:"Mountains  -  Girl",filePath:"songs/5.mp3",coverPath:"covers/5.jpg"},
+{songName:"Love Nevanty   -   Kaala Larka",filePath:"songs/6.mp3",coverPath:"covers/6.jpg"},
+{songName:"No Love  - Shubh",filePath:"songs/7.mp3",coverPath:"covers/7.jpg"},
+{songName:"We Rollin  - Shubh",filePath:"songs/8.mp3",coverPath:"./cover1.jpeg"},
+{songName:"Love  - Shubh",filePath:"songs/9.mp3",coverPath:"covers/1.jpg"},
+{songName:"Ek Din Pyaar  - McStan",filePath:"songs/9.mp3",coverPath:"covers/5.jpg"},
 
+
+
+{songName:"Hasti",filePath:"songs/10.mp3",coverPath:"covers/10.jpg"}
+]
+songItems.forEach((element, i)=>{
+element.getElementsByTagName("img")[0].src=songs[i].coverPath;
+element.getElementsByClassName("songName")[0].innerText=songs[i].songName;
+})
 // handel play pause click
 
 masterPlay.addEventListener("click",()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
 audioElement.play();
+masterPlay.classList.remove("fa-play-circle");
+masterPlay.classList.add("fa-pause-circle");
+gif.style.opacity=1;
+}
+else{
+    audioElement.pause();
+    masterPlay.classList.remove("fa-pause-circle");
+    masterPlay.classList.add("fa-play-circle");
+    gif.style.opacity=0;
     }
 })
-
-
 // listen to evenet
-myProgressbar.addEventListener("timeupdate", ()=>{
-console.log("time update");
+audioElement.addEventListener("timeupdate", ()=>{
+let Progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
+myProgressbar.value = Progress;
 })
+
+    myProgressbar.addEventListener("change",()=>{
+        audioElement.currentTime = myProgressbar.value * audioElement.duration/100;
+    })
+const makeAllPlays=()=>{
+    Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+        element.classList.remove("fa-pause-circle")
+        element.classList.add("fa-play-circle")
+
+    })
+}
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.addEventListener("click",(e)=>{
+makeAllPlays();
+e.target.classList.remove("fa-play-circle")
+e.target.classList.add("fa-pause-circle")
+    })
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
